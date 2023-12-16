ec7b672ae6039d43a9b35c314592e42a6e0d9093
pegawijaya <114848955+pegawijaya@users.noreply.github.com>
2023-12-16 20:34:42 +0700

    Delete serverkuliah directory

--- serverkuliah/deleteApi.js
+++
@@ -1,9 +0,0 @@
-const { response } = require("express");
-
-const mahasiswaNimToDelete = '1102028';
-
-fetch(`http://localhost:3000/mahasiswa/${mahasiswaNimToDelete}`, {
-    method: 'DELETE'
-}).then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error', error));
